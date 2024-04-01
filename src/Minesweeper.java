e5daa5ba49df764985ca2e81f500646a689da1f2
Setare Izadi <s232629@dtu.dk>
2024-04-01 23:20:19 +0200

    tilføjet panels

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -5,7 +5,64 @@ import java.util.Random;
 import javax.swing.*;
 
 public class Minesweeper {
+
+    private class MineTile extends JButton {
+        int r;
+        int c;
+
+        public MineTile(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }           
+    }
+
+    int tileSize = 70;
+    int numRows = 8;
+    int numCols = numRows;
+    int boardWidth = tileSize * numCols;
+    int boardHeight = tileSize * numRows;
+
+    JFrame frame = new JFrame("Minesweeper");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    MineTile[][] board = new MineTile[numRows][numCols];
+
+
     Minesweeper() {
         frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        textLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Minesweeper");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(numRows, numCols)); // 8x8 grid
+        // Sæt farven for baggrunden:
+        // boardPanel.setBackground(Color.BLACK);
+        frame.add(boardPanel);
+
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                MineTile tile = new MineTile(r, c);
+                board[r][c] = tile;
+
+                tile.setFocusable(false);
+                tile.setMargin(new Insets(0,0,0,0));
+                tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+                tile.setText("1");
+                boardPanel.add(tile);
+            }
+        }
     }
 }
