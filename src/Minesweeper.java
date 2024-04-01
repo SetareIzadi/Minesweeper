ecdbe392c66e03750fb37e81f84991788cd22d98
Setare Izadi <s232629@dtu.dk>
2024-04-01 22:14:25 +0200

    Indsat main og minesweeper class

---
+++ src/Minesweeper.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class Minesweeper {
+    Minesweeper() {
+        frame.setVisible(true);
+    }
+}
